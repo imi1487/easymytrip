73faa83bfec9b6e6c95737d652ce309a8ca70b0a
Imran <imranhetwade1487@gmail.com>
2024-05-15 10:55:29 +0530

    Code refactor

---
+++ src/main/java/easymytrip/example/easymytrip/railway.java
@@ -0,0 +1,14 @@
+package easymytrip.example.easymytrip;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class railway {
+
+    @GetMapping("/railway")
+    public String getData() {return "please book from air india 25% discount"; }
+}
+
