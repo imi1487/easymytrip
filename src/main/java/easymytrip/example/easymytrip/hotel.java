f446da411c0ab8f18fd1883e5d2f9c8545aa5ea5
Imran <imranhetwade1487@gmail.com>
2024-05-15 11:05:03 +0530

    hello

---
+++ src/main/java/easymytrip/example/easymytrip/hotel.java
@@ -0,0 +1,13 @@
+package easymytrip.example.easymytrip;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class hotel {
+
+    @GetMapping("/hotel")
+    public String getData() {return "please book hotel from easymytrip"; }
+}
