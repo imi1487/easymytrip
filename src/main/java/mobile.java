19afbb31e76a91d61e71a57da58b9272e7ee7279
imi1487 <152406865+imi1487@users.noreply.github.com>
2024-05-25 23:45:51 +0530

    Update mobile.java

--- src/main/java/mobile.java
+++ src/main/java/mobile.java
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class mobile {
 
     @GetMapping("/mobile")
-    public String getData() {return "i have different types of mobiles"; }
+    public String getData() {return "i have different types of mobiles like iphone, samsung"; }
 }
