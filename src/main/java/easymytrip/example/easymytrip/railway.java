1274068c03c39d1f3c205a7a82a04bf1a3bf6def
Imran <imranhetwade1487@gmail.com>
2024-05-25 22:51:21 +0530

    hello

--- src/main/java/easymytrip/example/easymytrip/railway.java
+++ src/main/java/easymytrip/example/easymytrip/railway.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class railway {
 
     @GetMapping("/railway")
-    public String getData() {return "please book vandhe bharat at free of cost"; }
+    public String getData() {return "please book vandhe bharat at free of costgit "; }
 }
 
