ba6d5cff02467e97338c9e841e9fe21778925258
Imran <imranhetwade1487@gmail.com>
2024-06-11 16:42:59 +0530

    login added

---
+++ src/main/java/easymytrip/example/easymytrip/login.java
@@ -0,0 +1,13 @@
+package easymytrip.example.easymytrip;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class login {
+
+    @GetMapping("/login")
+    public String getData() {return "hotels are free from today till end of this month & year"; }
+}
