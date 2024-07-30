5d0de8c71084222cfd63353c47461c57b22bb65d
Imran <imranhetwade1487@gmail.com>
2024-07-30 15:18:31 +0530

    new ms added

---
+++ src/main/java/easymytrip/example/easymytrip/flight.java
@@ -0,0 +1,10 @@
+package easymytrip.example.easymytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flight {
+    @GetMapping("/flight")
+    public String getData() {return  "Please book your flight ticket at  45% discount" ; }
+}
---
+++ src/main/java/easymytrip/example/easymytrip/hotel.java
@@ -0,0 +1,10 @@
+package easymytrip.example.easymytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+    @GetMapping("/hotel")
+    public String getData() {return  "Please book your hotel at 25 % discount" ; }
+}
