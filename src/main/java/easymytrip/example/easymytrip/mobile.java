f0c939b39b4c7adc3d637249db3a8b66fc32f00e
Imran <imranhetwade1487@gmail.com>
2024-08-09 11:40:47 +0530

    new

--- src/main/java/easymytrip/example/easymytrip/mobile.java
+++ src/main/java/easymytrip/example/easymytrip/mobile.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class mobile {
     @GetMapping("/mobile")
-    public String getData() {return  "New cell phone has been launched today & tomorrow samsung" ; }
+    public String getData() {return  "New cell phone has been launched today & tomorrow samsung, iphone" ; }
 }
