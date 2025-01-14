616e4ca9a81fd0dc12c6401a742d76deb7c3557a
imi1487 <152406865+imi1487@users.noreply.github.com>
2025-01-14 12:47:43 +0530

    Update hotel.java

--- src/main/java/easymytrip/example/easymytrip/hotel.java
+++ src/main/java/easymytrip/example/easymytrip/hotel.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class hotel {
     @GetMapping("/hotel")
-    public String getData() {return  "book your hotel at free " ; }
+    public String getData() {return  "book your hotel at 30 % discount " ; }
 }
