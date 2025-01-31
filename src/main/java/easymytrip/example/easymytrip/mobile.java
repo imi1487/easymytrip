b7f07d19262f2463ae2284cdf9c18b83a70917b1
Imran <imranhetwade1487@gmail.com>
2025-01-31 13:20:11 +0530

    new

--- src/main/java/easymytrip/example/easymytrip/mobile.java
+++ src/main/java/easymytrip/example/easymytrip/mobile.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class mobile {
     @GetMapping("/mobile")
-    public String getData() {return  "i will buy i phone 21220 this month " ; }
+    public String getData() {return  "i will buy i pho 889969660 this month " ; }
 }
